8489282b1cb08e3c51e6a8616a26b3f5589365f1
MrDave <davidschnoll@gmail.com>
2023-11-01 22:00:05 +0200

    Reduce queries for comments count on index

--- blog/views.py
+++ blog/views.py
@@ -21,6 +21,20 @@ def serialize_post(post):
     }
 
 
+def serialize_post_optimized(post):
+    return {
+        'title': post.title,
+        'teaser_text': post.text[:200],
+        'author': post.author.username,
+        'comments_amount': post.comments_count,
+        'image_url': post.image.url if post.image else None,
+        'published_at': post.published_at,
+        'slug': post.slug,
+        'tags': [serialize_tag(tag) for tag in post.tags.all()],
+        'first_tag_title': post.tags.all()[0].title,
+    }
+
+
 def serialize_tag(tag):
     return {
         'title': tag.title,
@@ -30,7 +44,10 @@ def serialize_tag(tag):
 
 def index(request):
     
-    all_posts = Post.objects.annotate(likes_count=Count("likes")).prefetch_related("author")
+    all_posts = Post.objects.annotate(
+        likes_count=Count("likes", distinct=True),
+        comments_count=Count("comments", distinct=True)
+    ).prefetch_related("author")
     most_popular_posts = all_posts.order_by("-likes_count")[:5]
 
     most_fresh_posts = all_posts.order_by('-published_at')[:5]
@@ -39,9 +56,9 @@ def index(request):
 
     context = {
         'most_popular_posts': [
-            serialize_post(post) for post in most_popular_posts
+            serialize_post_optimized(post) for post in most_popular_posts
         ],
-        'page_posts': [serialize_post(post) for post in most_fresh_posts],
+        'page_posts': [serialize_post_optimized(post) for post in most_fresh_posts],
         'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
     }
     return render(request, 'index.html', context)
